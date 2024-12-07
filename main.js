2c4811d043e38fadde710f9ec211a640e27cffca
David Paparo <d.paparo@tiscali.it>
2024-12-07 12:41:56 +0100

    carico esercizio svolto

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 
-let saluto = `Ciao`
+let gatti_tot = 106;
 
-console.log(saluto);
+let gatti_in_fila = 10;
+
+let file = Math.floor(gatti_tot / gatti_in_fila);
+
+let gatti_extra = gatti_tot % gatti_in_fila;
+
+let gatti_mancanti = gatti_in_fila - gatti_extra;
+
+
+console.log( `Gatti totali ${gatti_tot}, gatti per fila ${gatti_in_fila}` );
+
+
+console.log(`Ci sono ${file} file di gatti e ne mancano ${gatti_mancanti} per una nuova fila, con un avanzo di ${gatti_extra} gatti`);
